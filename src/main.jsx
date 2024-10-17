74e5bb3fab2fde86e2b565521535325d3ec04eb8
Loficrypto <lofiking810@gmail.com>
2024-10-17 18:07:58 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { analytics } from './firebaseConfig';
+import { logEvent } from 'firebase/analytics';
+
+logEvent(analytics, 'notification_received');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
