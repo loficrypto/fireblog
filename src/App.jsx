8e9cd39c649a24d05d8be2348e72573e7dda9fd1
Loficrypto <lofiking810@gmail.com>
2024-10-17 17:33:07 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 // App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Blog from './components/Blog';
 import Admin from './components/Admin';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import Post from './components/Post';
+import Navbar from './components/Navbar';
+import { analytics } from './firebaseConfig';
+import { logEvent } from 'firebase/analytics';
 
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    logEvent(analytics, 'page_view', { page_path: location.pathname });
+  }, [location]);
+
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Blog />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/admin" element={<Admin />} />
+        </Route>
+        <Route path="/post/:id" element={<Post />} />
       </Routes>
     </Router>
   );
