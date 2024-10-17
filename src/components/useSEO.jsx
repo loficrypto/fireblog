f0495100425d6d9c2de40acef457b3fa1375d06d
Loficrypto <lofiking810@gmail.com>
2024-10-17 17:35:07 +0600

    Create useSEO.jsx

---
+++ src/components/useSEO.jsx
@@ -0,0 +1,20 @@
+// useSEO.jsx
+import { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+
+const useSEO = ({ title, description, keywords }) => {
+  useEffect(() => {
+    document.title = title;
+    document.head.querySelector('meta[name="description"]').content = description;
+    document.head.querySelector('meta[name="keywords"]').content = keywords;
+  }, [title, description, keywords]);
+
+  return (
+    <Helmet>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+    </Helmet>
+  );
+};
+
+export default useSEO;
