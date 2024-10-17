9419602259d0fcbc583cc85c41cdebc89cf8d59f
Loficrypto <lofiking810@gmail.com>
2024-10-17 20:28:01 +0600

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './firebaseConfig';
+import { auth } from '../firebaseConfig';
 
 const PrivateRoute = () => {
   const [user, loading] = useAuthState(auth);
