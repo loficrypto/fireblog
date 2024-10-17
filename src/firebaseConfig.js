f3877d8d6a1f49a3ec9651ce8c6a31f576391c07
Loficrypto <lofiking810@gmail.com>
2024-10-17 19:28:23 +0600

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,14 +5,16 @@ import { getAnalytics } from 'firebase/analytics';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyCIwkNBm9ajrZZ6je6o5JahukaVurhKFaQ",
+  authDomain: "fire4blog.firebaseapp.com",
+  databaseURL: "https://fire4blog-default-rtdb.firebaseio.com",
+  projectId: "fire4blog",
+  storageBucket: "fire4blog.appspot.com",
+  messagingSenderId: "591429070983",
+  appId: "1:591429070983:web:228d44bdfe66a634e44822",
+  measurementId: "G-2TW7B8EEH1"
 };
 
 const app = initializeApp(firebaseConfig);
