0c2667773ca0e41c549fa4b48b4017bcb0dd24a5
Loficrypto <lofiking810@gmail.com>
2024-10-17 17:32:13 +0600

    Create PrivateRoute.jsx

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+// PrivateRoute.jsx
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebaseConfig';
+
+const PrivateRoute = () => {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) return <div>Loading...</div>;
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
